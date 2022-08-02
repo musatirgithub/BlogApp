385c33b6236e917188b3dfcad6fc5202aea5e070
Mehmet Ugur SATIR <musatir@gmail.com>
2022-08-03 01:22:48 +0300

    profile page was updated

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,30 @@
-import React from 'react'
+//import { useContext } from "react";
+
+import BlogCard from "../components/BlogCard";
+import { useFetch } from "../helpers/firestoreFunctions";
+import {Grid} from '@mui/material';
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const { isLoading, blogs } = useFetch();
+  const {currentUser}= useContext(AuthContext);
 
-export default Profile
+  return isLoading ? (<div>Loading...</div>
+  ) : (
+    <Grid container spacing={2} 
+    justifyContent="center" 
+    alignItems="center"
+    sx={{marginTop:'3rem'}}>
+{    blogs.filter((item)=>item.uid === currentUser.uid).map((blog)=> {
+      return (
+        <Grid item key={blog.id} xs={12} sm={6} md={4}>
+          <BlogCard blog={blog} />
+        </Grid>
+      )
+    })}
+
+  </Grid>);
+};
+
+export default Profile;
